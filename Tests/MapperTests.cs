eb65e5dc29c7b49b87fbdd6397d70b52aeb04607
Stanko Culaja <culaja@gmail.com>
2018-04-22 21:19:36 +0200

    Added first test to the solution

---
+++ Tests/MapperTests.cs
@@ -0,0 +1,12 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class MapperTests
+    {
+        [Fact]
+        public void first_test_in_the_application() =>
+            "SomeString".Should().Be("SomeString");
+    }
+}
